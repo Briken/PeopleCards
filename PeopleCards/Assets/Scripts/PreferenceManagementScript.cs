deaca6e23a7749159b6f69b8d2e8d9d3773eb848
Briken <brokenbriken@gmail.com>
2021-11-16 16:33:17 +0000

    Adding basic swipe control

--- PeopleCards/Assets/Scripts/PreferenceManagementScript.cs
+++ PeopleCards/Assets/Scripts/PreferenceManagementScript.cs
@@ -5,17 +5,26 @@ using UnityEngine.Android;
 
 public class PreferenceManagementScript : MonoBehaviour
 {
+    public GameObject nextButton;
+    public GameObject prevButton;
     // Start is called before the first frame update
     void Start()
     {
-        #if PLATFORM_ANDROID
+        if(Application.platform == RuntimePlatform.Android){
+            deactivateButtons();
             if (!Permission.HasUserAuthorizedPermission(Permission.ExternalStorageRead)||
                 !Permission.HasUserAuthorizedPermission(Permission.ExternalStorageWrite))
             {
                 Permission.RequestUserPermission(Permission.ExternalStorageRead);
                 Permission.RequestUserPermission(Permission.ExternalStorageWrite);
             }
-        #endif
+        }
+    }
+
+    private void deactivateButtons()
+    {
+        nextButton.SetActive(false);
+        prevButton.SetActive(false);
     }
 
     // Update is called once per frame
---
+++ PeopleCards/Assets/Scripts/SwipeControlScript.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwipeControlScript : MonoBehaviour
+{
+    private Vector2 startTouchPos, endTouchPos;
+    private CardSwapperScript swapper;
+
+    void Start()
+    {
+        swapper = GetComponent<CardSwapperScript>();
+    } 
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+        {
+            startTouchPos = Input.GetTouch(0).position;
+        }
+
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Ended)
+        {
+            endTouchPos = Input.GetTouch(0).position;
+        }
+
+        if (endTouchPos.x < startTouchPos.x && Input.GetTouch(0).phase == TouchPhase.Ended)
+        {
+            swapper.DisplayPrevCard();
+            nullTouchPos();
+        }
+        if (endTouchPos.x > startTouchPos.x && Input.GetTouch(0).phase == TouchPhase.Ended)
+        {
+            swapper.DisplayNextCard();
+            nullTouchPos();
+        }
+    }
+
+    private void nullTouchPos()
+    {
+            startTouchPos =new Vector2();
+            endTouchPos = new Vector2();
+    }
+}
